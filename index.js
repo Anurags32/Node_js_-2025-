3e03a7f6922d1eacce1eb57bbc7688bbd9765f93
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-12 09:33:16 +0530

    some chnages

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const math = require('./math');
 
 console.log(math.add(5, 10));
 console.log(math.sub(10, 5));
-console.log(math.mult(4, 9));
+console.log(math.malt(4, 9));
 console.log(math.div(27, 9));
 console.log(math.modulo(289, 27));
