b1293c78f7fba8ab6c59f6b33818ecbed1564d95
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-17 20:32:32 +0530

    practis event

--- event.js
+++ event.js
@@ -3,6 +3,14 @@ const EventEmitter = require("events");
 // create instance of event emitter
 const emitter = new EventEmitter();
 // define event listener
+const eventcounter = {
+    "user-login": 0,
+    "user-logout": 0,
+    "user-purches": 0,
+    "profile-update": 0,
+
+}
+
 
 emitter.on("greet", () => {
     console.log("anurag tiwari");
@@ -10,6 +18,20 @@ emitter.on("greet", () => {
 emitter.on("add", () => {
     var ab = 4 + 6;
     console.log(ab);
-})
+});
+emitter.on("user-login", (username) => {
+    console.log("${username} loged in")
+});
+emitter.on("user-logout", () => {
+
+});
+emitter.on("user-purches", () => {
+
+});
+emitter.on("profile-update", () => { });
 emitter.emit("greet");
 emitter.emit("add");
+emitter.emit("user-login", "Anurag");
+emitter.emit("user-logout", "Anurag");
+emitter.emit("user-purches", "Anurag", "MotoG85");
+emitter.emit("profile-update", "Anurag Tiwari");
